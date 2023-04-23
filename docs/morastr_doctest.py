8abbad5ff04a3402d2dc8db97b4750452e8692de
Hizuru <106918920+Hizuru3@users.noreply.github.com>
2023-04-23 23:07:34 +0900

    Add files via upload

---
+++ docs/morastr_doctest.py
@@ -0,0 +1,20 @@
+import doctest
+import sys
+
+import morastrja
+
+
+globs = dict(vars(morastrja))
+globs.update(vars(morastrja.utils))
+
+flags = (doctest.IGNORE_EXCEPTION_DETAIL
+       | doctest.REPORT_NDIFF
+       | doctest.FAIL_FAST)
+
+targets = [r"../README.md", r"_sources/morastr_module.rst.txt"]
+
+for target in targets:
+    failures, _ = doctest.testfile(
+        target, globs=globs, optionflags=flags)
+
+    if failures: sys.exit(1)
